b562ca5d8cc30b8430aae805f20e295b3d1ccef2
Romil Shah <romilshah2199@gmail.com>
2021-10-26 07:50:17 +0530

    added-user-model

---
+++ src/main/java/com/learningbybuilding/resumeportal/security/models/User.java
@@ -0,0 +1,22 @@
+package com.learningbybuilding.resumeportal.security.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+}
