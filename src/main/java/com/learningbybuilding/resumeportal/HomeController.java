bac6890a42576d2d9aecd2779eb0c56905b82d51
Romil Shah <romilshah2199@gmail.com>
2021-10-26 23:23:18 +0530

    added link

--- src/main/java/com/learningbybuilding/resumeportal/HomeController.java
+++ src/main/java/com/learningbybuilding/resumeportal/HomeController.java
@@ -1,15 +1,13 @@
 package com.learningbybuilding.resumeportal;
 
+import com.learningbybuilding.resumeportal.profile.Education;
 import com.learningbybuilding.resumeportal.profile.Job;
 import com.learningbybuilding.resumeportal.profile.UserProfile;
 import com.learningbybuilding.resumeportal.profile.UserProfileRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -42,10 +40,27 @@ public class HomeController {
     }
 
     @GetMapping("/view/{userName}")
-    public String view(@PathVariable String userName, Model model) {
+    public String view(@PathVariable String userName, Model model, Principal principal) {
         UserProfile userProfile = userProfileRepository.findByUserName(userName)
-                        .orElseThrow(() -> new RuntimeException(userName + "not found"));
+                .orElseThrow(() -> new RuntimeException(userName + "not found"));
         model.addAttribute("userProfile", userProfile);
-        return "resume-templates/" + userProfile.getTheme() +"/index";
+        String currentUser = principal != null ? principal.getName() : null;
+        model.addAttribute("currentUser", currentUser);
+        return "resume-templates/" + userProfile.getTheme() + "/index";
     }
+
+    /*@PostMapping("/add")
+    public String addARow(@ModelAttribute("userProfile") UserProfile userProfile, @RequestParam("add") String toBeAdded,
+                          Model model) {
+        System.out.println(userProfile.getSkills());
+        if (toBeAdded.equals("job")) {
+            userProfile.getJobs().add(new Job());
+        } else if (toBeAdded.equals("edu")) {
+            userProfile.getEducations().add(new Education());
+        } else if (toBeAdded.equals("skill")) {
+            userProfile.getSkills().add("");
+        }
+        model.addAttribute("userProfile", userProfile);
+        return "edit-profile";
+    }*/
 }
