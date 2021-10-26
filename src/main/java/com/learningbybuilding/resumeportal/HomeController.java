b4131334788deb501d443cd16e0a0c2c1e07bee7
Romil Shah <romilshah2199@gmail.com>
2021-10-26 10:18:25 +0530

    added free html templates

--- src/main/java/com/learningbybuilding/resumeportal/HomeController.java
+++ src/main/java/com/learningbybuilding/resumeportal/HomeController.java
@@ -1,11 +1,13 @@
 package com.learningbybuilding.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String sayHello() {
         return "Hello";
     }
@@ -13,5 +15,10 @@ public class HomeController {
     @GetMapping("/edit")
     public String edit() {
         return "edit-page";
+    }*/
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "resume-templates/1/index";
     }
 }
